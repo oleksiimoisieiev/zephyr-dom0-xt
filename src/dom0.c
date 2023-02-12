4e42195071d23f73dda3ebadbae9d096c375b511
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2023-02-12 19:44:13 +0200

    dom0: initial commit
    This commit adds very initial support for Zephyr Dom0 App.
    
    It should be capable of creating exactly one domain - DomD. Domain
    config is hardcoded in domd_cfg.c file.
    
    It is based on "xenutils" project (https://github.com/firscity/xenutils),
    which was written by the following people:
    
    Co-authored-by: Author: Dmytro Firsov <dmytro_firsov@epam.com>
    Co-authored-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Co-authored-by: Mykola Perehudov <Mykola_Perehudov@epam.com>
    Co-authored-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

---
+++ src/dom0.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stddef.h>
+#include <xen_dom_mgmt.h>
+
+/* TODO: call xen library initialization routine */
+extern void init_root();
+
+void main(void)
+{
+	/* HACK */
+	char *argv[] = {"", "-d", "1"};
+
+	init_root();
+	domu_create(NULL, 3, argv);
+}
